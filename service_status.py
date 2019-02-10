ff66d490bac1bdd250639b328cc08c810bb70126
Valeria Shestakova <lerashestakova@yandex.ru>
2019-02-10 15:24:10 +0300

    Service_status: add background task

--- service_status.py
+++ service_status.py
@@ -1,7 +1,12 @@
 import yaml
 from aiohttp import web
+import asyncio
 import ipaddress
 import aiopg.sa
+import socket
+from contextlib import closing
+
+TIMEOUT = 0.1
 
 
 def get_config(path):
@@ -64,16 +69,36 @@ async def get_records_by_ip_and_port(request):
         return web.json_response(resp)
 
 
-def setup_routes(app):
-    app.router.add_get('/', index)
-    app.router.add_get('/service_status/get_records_by_ip/{ip}'
-                       , get_records_by_ip)
-    app.router.add_get('/service_status/get_records_by_ip_and_port/{ip}'
-                       '/{port}'
-                       , get_records_by_ip_and_port)
+async def update_db(app, available, id):
+    async with app['db'].acquire() as conn:
+        await conn.execute(
+            """UPDATE services SET available=(%s) WHERE id=(%s);""",
+            (available, id))
 
 
-async def init_pg(app):
+async def check_available(ip, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        sock.settimeout(TIMEOUT)
+        if sock.connect_ex((ip, port)) == 0:
+            return True
+        else:
+            return False
+
+
+async def update_service_available(app):
+    while True:
+        async with app['db'].acquire() as conn:
+            services = await conn.execute("""SELECT id, ip, port, available FROM
+                                             services;""")
+            async for service in services:
+                available = await check_available(service[1],
+                                                  service[2])
+                if service[3] is not available:
+                    await update_db(app, available, service[0])
+        await asyncio.sleep(30)
+
+
+async def start_background_tasks(app):
     conf = app['config']['postgres']
     engine = await aiopg.sa.create_engine(
         database=conf['database'],
@@ -83,19 +108,30 @@ async def init_pg(app):
         port=conf['port'],
     )
     app['db'] = engine
+    app['update_available'] = app.loop.create_task(update_service_available(app))
 
 
-async def close_pg(app):
+async def cleanup_background_tasks(app):
+    app['update_available'].cancel()
+    await app['update_available']
     app['db'].close()
     await app['db'].wait_closed()
+
+
+def setup_routes(app):
+    app.router.add_get('/', index)
+    app.router.add_get('/service_status/get_records_by_ip/{ip}'
+                       , get_records_by_ip)
+    app.router.add_get('/service_status/get_records_by_ip_and_port/{ip}'
+                       '/{port}'
+                       , get_records_by_ip_and_port)
 
 
 if __name__ == '__main__':
     app = web.Application()
     config = get_config('config.yaml')
     app['config'] = config
-    app.on_startup.append(init_pg)
-    app.on_cleanup.append(close_pg)
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
     setup_routes(app)
     web.run_app(app)
-
